fa2bda24421e0db9929d943170ce5e88d4adc04f
Pritam Gawade <pritamgawade@Pritams-MacBook-Pro.local>
2023-02-24 11:42:47 -0800

    first commit

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const onSaveExpenseHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+    props.onAddExpense(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={onSaveExpenseHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
